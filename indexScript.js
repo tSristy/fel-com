69f3e445ea0a3d0f737c83b59008c26db26eff4b
tSristy <hoque.tasnim05@gmail.com>
2022-07-31 13:07:23 +0600

    first push

---
+++ indexScript.js
@@ -0,0 +1,41 @@
+document.getElementById("btn-smartplaza-location").addEventListener('click',function(){
+    const carry_id = document.getElementById("btn-smartplaza-location").value;
+    console.log(carry_id,"clicked");
+})
+
+/*---------------------------------------------------------------------------------------------*/
+/*------------------------------------KNOW MORE BY CLICKING------------------------------------*/
+/*---------------------------------------------------------------------------------------------*/
+document.getElementById("btn-more-mobile").addEventListener('click',function(){
+    console.log("clicked to know more about mobile");
+    window.open("test.html",'_self');
+})
+
+document.getElementById("btn-more-television").addEventListener('click',function(){
+    console.log("clicked to know more about television");
+})
+
+document.getElementById("btn-more-freeze").addEventListener('click',function(){
+    console.log("clicked to know more about freeze");
+})
+
+document.getElementById("btn-more-air").addEventListener('click',function(){
+    console.log("clicked to know more about air");
+})
+
+document.getElementById("btn-more-washing").addEventListener('click',function(){
+    console.log("clicked to know more about washing");
+})
+
+document.getElementById("btn-more-oven").addEventListener('click',function(){
+    console.log("clicked to know more about oven");
+})
+
+document.getElementById("btn-more-ac").addEventListener('click',function(){
+    console.log("clicked to know more about ac");
+})
+
+document.getElementById("btn-more-appliance").addEventListener('click',function(){
+    console.log("clicked to know more about appliance");
+})
+
